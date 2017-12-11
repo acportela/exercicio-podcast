5a21c7f90b95541fab85934ea833047e58268f92
Antonio Rodrigues <acpr@Antonios-MacBook-Pro.local>
2017-12-11 06:37:52 -0300

    Add o UIAutomator

--- Podcast/app/src/androidTest/java/br/ufpe/cin/if710/podcast/MainActivityTests.java
+++ Podcast/app/src/androidTest/java/br/ufpe/cin/if710/podcast/MainActivityTests.java
@@ -1,8 +1,14 @@
 package br.ufpe.cin.if710.podcast;
 
 import android.os.Build;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.UiSelector;
+import android.util.Log;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -11,6 +17,7 @@ import org.junit.runner.RunWith;
 
 import br.ufpe.cin.if710.podcast.ui.MainActivity;
 
+import static android.content.ContentValues.TAG;
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.InstrumentationRegistry.getTargetContext;
 import static android.support.test.espresso.Espresso.onData;
@@ -19,13 +26,10 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.hamcrest.CoreMatchers.anything;
 
 
-/**
- * Instrumentation test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTests {
+
+    UiDevice mDevice;
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
@@ -33,27 +37,27 @@ public class MainActivityTests {
     @Before
     public void setup() throws InterruptedException {
 
-        //Trecho usado para dar permissão ao teste para baixar o arquivo
-        //Para verificar seu efeito, se comentado, o botão
-        //não muda o texto para "Baixando" e exibe o diolog de permissão
-        //Se descomentado, o dialog também é exibido, mas antes vemos o texto do
-        //botão mudar. O app em si não pergunta novamente se já tiver permissão
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        //Gambiarra para prover a permissão pela shell
+        //Deixei aqui só por conhecimento
+        //Há ma melhor alternativa mais abaixo
+        /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             getInstrumentation().getUiAutomation().executeShellCommand(
                     "pm grant " + getTargetContext().getPackageName()
                             + " android.permission.WRITE_EXTERNAL_STORAGE");
-        }
+        }*/
 
-        //Necessário para esperar a lista ser carregada. Caso falhe, aumente o tempo
+        mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+        //Necessário para esperar a lista ser carregada.
         Thread.sleep(3000);
     }
 
     //Testa o click no botao de "Baixar" ou "Tocar de um item da lista (neste caso o primeiro)
     //Testa o uso da permissão de salvar arquivos na memória externa
-    //Comente o trecho no before para checar a permissão
     @Test
-    public void itemClickMainButtonTest(){
-        onData(anything()).inAdapterView(withId(R.id.items)).atPosition(0).onChildView(withId(R.id.item_action)).perform(click());
+    public void itemClickMainButtonTest() throws InterruptedException {
+        onData(anything()).inAdapterView(withId(R.id.items)).atPosition(1).onChildView(withId(R.id.item_action)).perform(click());
+        Thread.sleep(2000);
+        allowPermissionsIfNeeded();
     }
 
     //Testa o click no item para abrir a activity de detalhes
@@ -62,4 +66,18 @@ public class MainActivityTests {
         onData(anything()).inAdapterView(withId(R.id.items)).atPosition(0).perform(click());
     }
 
+    //Só funciona para locale EN devido ao nome do botão
+    //É só trocar o texto caso necessário
+    private void allowPermissionsIfNeeded()  {
+        if (Build.VERSION.SDK_INT >= 23) {
+            UiObject allowPermissions = mDevice.findObject(new UiSelector().text("ALLOW"));
+            if (allowPermissions.exists()) {
+                try {
+                    allowPermissions.click();
+                } catch (UiObjectNotFoundException e) {
+                    Log.d(TAG, "allowPermissionsIfNeeded: no permissions with this text");
+                }
+            }
+        }
+    }
 }
